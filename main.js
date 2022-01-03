f92e008bf33f1cb6ac88fdb44014ac5c17f3fe4c
Omar <omarsalem0721@gmail.com>
2022-01-03 16:01:39 +0200

    Add a new book function

--- main.js
+++ main.js
@@ -1,28 +1,53 @@
 const booksList = [
   {
+    id: 0,
     author: "Benjamin Semah",
     title: "Become a Better Programmer"
   },
   {
+    id: 1,
     author: "Omar Salem",
     title: "How to Succeed in Programming"
-  }
+  },
 ];
 
-const books = document.querySelector('.books');
 
-let booksHtml = ``;
 
-booksList.forEach((book) => {
-  booksHtml = 
-    booksHtml + 
-    `<li class="book-item">
-      <p class="book-title">${book.title}</p>
-      <p class="book-author">${book.author}</p>
-      <button class="remove-btn" type="button">Remove</button>
-      <hr>
-    </li>
-    `
-});
+function create() {
+  let books = document.querySelector('.books');
+  let booksHtml = ``;
+  booksList.forEach((book) => {
+    booksHtml =
+      booksHtml +
+      `<li class="book-item">
+        <p class="book-title">${book.title}</p>
+        <p class="book-author">${book.author}</p>
+        <button class="remove-btn" type="button">Remove</button>
+        <hr>
+      </li>
+      `
+  });
+  books.innerHTML = booksHtml;
+}
 
-books.innerHTML = booksHtml;
+create();
+
+// add book functionality
+const addBtn = document.querySelector('.add-btn');
+addBtn.addEventListener('click', () => {
+  const title = document.querySelector('.input-title').value.trim();
+  const author = document.querySelector('.input-author').value.trim();
+  const newBook = { id: Math.floor(Math.random() * 100000) + 2, author: author, title: title }
+  addBook(newBook);
+
+  console.log(booksList)
+})
+
+function addBook(book) {
+  booksList.push(book);
+  // add book to Dom
+  create();
+}
+
+
+
